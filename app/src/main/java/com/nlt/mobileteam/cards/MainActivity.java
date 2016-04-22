2a61ef2038f0ad39fae86b786760a2e6741caa72
chickinnick <chickinnick@gmail.com>
2016-04-22 23:28:50 +0300

    Initial commit

---
+++ app/src/main/java/com/nlt/mobileteam/cards/MainActivity.java
@@ -0,0 +1,164 @@
+package com.nlt.mobileteam.cards;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+
+
+    private JazzyViewPager mJazzy;
+
+
+
+
+
+    private void setupJazziness(JazzyViewPager.TransitionEffect effect) {
+        mJazzy = (JazzyViewPager) findViewById(R.id.jazzy_pager);
+        mJazzy.setTransitionEffect(effect);
+        mJazzy.setAdapter(new MainAdapter());
+       // mJazzy.setPageMargin(30);
+    }
+
+    private class MainAdapter extends PagerAdapter {
+        @Override
+        public Object instantiateItem(ViewGroup container, final int position) {
+            TextView text = new TextView(MainActivity.this);
+            text.setGravity(Gravity.CENTER);
+            text.setTextSize(30);
+            text.setTextColor(Color.WHITE);
+            text.setText("Page " + position);
+            text.setPadding(30, 30, 30, 30);
+            int bg = Color.rgb((int) Math.floor(Math.random()*128)+64,
+                    (int) Math.floor(Math.random()*128)+64,
+                    (int) Math.floor(Math.random()*128)+64);
+            text.setBackgroundColor(bg);
+            container.addView(text, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            mJazzy.setObjectForPosition(text, position);
+            return text;
+        }
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object obj) {
+            container.removeView(mJazzy.findViewFromObject(position));
+        }
+        @Override
+        public int getCount() {
+            return Integer.MAX_VALUE;
+        }
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            if (view instanceof OutlineContainer) {
+                return ((OutlineContainer) view).getChildAt(0) == obj;
+            } else {
+                return view == obj;
+            }
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_main);
+        setupJazziness(JazzyViewPager.TransitionEffect.FlipHorizontal);
+      /*  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        menu.add("Toggle Fade");
+        String[] effects = this.getResources().getStringArray(R.array.jazzy_effects);
+        for (String effect : effects)
+            menu.add(effect);
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (item.getTitle().toString().equals("Toggle Fade")) {
+            mJazzy.setFadeEnabled(!mJazzy.getFadeEnabled());
+        } else {
+            JazzyViewPager.TransitionEffect effect = JazzyViewPager.TransitionEffect.valueOf(item.getTitle().toString());
+            setupJazziness(effect);
+        }
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ lib/src/main/java/com/jfeinstein/jazzyviewpager/MainActivity.java
@@ -0,0 +1,20 @@
+package com.jfeinstein.jazzyviewpager;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.TextView;
+
+import com.jfeinstein.jazzyviewpager.JazzyViewPager.TransitionEffect;
+
+public class MainActivity extends Activity {
+
+
+}
